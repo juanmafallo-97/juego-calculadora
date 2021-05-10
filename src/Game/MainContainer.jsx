31685ee2750242fbad79ce3a5c7a4725634721c3
Juanma <juanmafallo@gmail.com>
2021-05-10 16:14:30 -0300

    .

--- src/Game/MainContainer.jsx
+++ src/Game/MainContainer.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { resetGame } from '../actions';
 import OnGame from './OnGame';
 
 export default function MainContainer() {
+  const dispatch = useDispatch();
   const [playing, setPlaying] = useState(false);
   return (
     <div className="main-container">
       {playing ? (
         <div>
           <OnGame />
-          <button onClick={() => setPlaying(false)}>Volver</button>
+          <button
+            onClick={() => {
+              dispatch(resetGame());
+              setPlaying(false);
+            }}
+          >
+            Volver
+          </button>
         </div>
       ) : (
         <button onClick={() => setPlaying(true)}>Jugar!</button>
